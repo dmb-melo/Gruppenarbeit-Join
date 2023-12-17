40f21deb15d6bee44a536b0d5db3b271957028ca
Silvan Stuber <silvan.stuber93@gmail.com>
2023-12-17 22:55:03 +0100

    Password input field revised and remember me included

--- js/loginpage.js
+++ js/loginpage.js
@@ -1,6 +1,10 @@
 /**
  * This function opens and closes the submenu and the closeSubmenu() function must be added to the body
+ * @function
+ * @name showSubmenu
  *
+ * @type {boolean}
+ * @description submenuIsOpen specifies whether the submenu is open or closed. With the value false, we define the submenu as closed
  */
 
 let submenuIsOpen = false;
@@ -21,25 +25,82 @@ function closeSubmenu() {
   }
 }
 
+/**
+ *This function prevents the submenu from being closed if the submenu is clicked on
+ *
+ * @function
+ * @name doNotClose
+ */
 function doNotClose(event) {
   event.stopPropagation();
 }
 
+/**
+ *Animation of the join logo when the start page is loaded
+ *
+ * @function
+ * @name startAnimation
+ */
 function initLoading() {
   startAnimation();
 }
 function startAnimation() {
-  // Animation immer starten
-  document.querySelector('.animationJoinLogo').classList.add('animated');
-  document.querySelector('.animationJoinLogo').classList.remove('dNone');
-  document.querySelector('.join-logo').classList.add('animated');
+  document.querySelector(".animationJoinLogo").classList.add("animated");
+  document.querySelector(".animationJoinLogo").classList.remove("dNone");
+  document.querySelector(".join-logo").classList.add("animated");
 }
 
-function myFunction() {
-  var x = document.getElementById("passwordInput");
-  if (x.type === "password") {
-    x.type = "text";
+/**
+ * Updates the image next to the password input field based on the field's content.
+ *
+ * When the password input field is empty, the image is set to a lock symbol.
+ * If the field is not empty, the image is changed to a visibility symbol (eye icon).
+ * Additionally, logs the current value of the password field to the console.
+ *
+ * The value of the password input field is stored in the variable `inputValuePassword `.
+ *
+ * @type {boolean}
+ * @description The global variable `passwordIsVisible` defines whether the password is visible or not
+ *
+ * @function
+ * @name passwordInputLogIn
+ */
+let passwordIsVisible = false;
+function passwordInputLogIn() {
+  let inputValuePassword = document.getElementById("passwordInput").value;
+  if (inputValuePassword) {
+    if (!passwordIsVisible) {
+      document.getElementById("passwordImg").src = "./assets/img/visibility_off.svg";
+    } else {
+      document.getElementById("passwordImg").src = "./assets/img/visibility.svg";
+    }
   } else {
-    x.type = "password";
+    document.getElementById("passwordImg").src = "./assets/img/lock.svg";
+  }
+}
+
+/**
+ *Makes the entered value in the password input field visible and changes the SVG, furthermore the type of the input field is changed
+ * @function
+ * @name passwordVisible
+ *
+ * The value of the password input field is stored in the variable `inputValuePassword `.
+ *
+ * @type {boolean}
+ * @description The global variable `passwordIsVisible` defines whether the password is visible or not
+ */
+function passwordVisible() {
+  let x = document.getElementById("passwordInput");
+  let inputValuePassword = document.getElementById("passwordInput").value;
+  if (inputValuePassword) {
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("passwordImg").src = "./assets/img/visibility.svg";
+      passwordIsVisible = true;
+    } else {
+      x.type = "password";
+      document.getElementById("passwordImg").src = "./assets/img/visibility_off.svg";
+      passwordIsVisible = false;
+    }
   }
 }
