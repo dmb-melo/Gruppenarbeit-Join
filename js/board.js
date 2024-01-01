2e4dc43c1d0a697b6fa207b285830bccff5f5293
Silvan Stuber <silvan.stuber93@gmail.com>
2024-01-01 21:14:44 +0100

    css link to summary

--- js/board.js
+++ js/board.js
@@ -1,12 +1,7 @@
-async function boardInit() {
-    await includeHTML();
-    render();
-}
 
 function renderBoardHTML() {
     document.getElementById('contentJoin').innerHTML = ``;
     document.getElementById('contentJoin').innerHTML = generateBoardHTML();
-    boardInit();
 }
 
 
