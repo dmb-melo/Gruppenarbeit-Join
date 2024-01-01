cb9f0818b9d4f03bf6cc0fedb1cc4478dc5cda4b
Bekir Labjani <127668660+BekirLabjani@users.noreply.github.com>
2024-01-01 13:45:35 +0100

    reset

--- js/saveALoad.js
+++ js/saveALoad.js
@@ -1,24 +1,2 @@
-const STORAGE_TOKEN = 'XULVXKXQ87YFSN0Q9PFZSMP577RV8CAJX896XQXQ';
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
-
-
-let tasks = [];
-
-
-async function currentUserTaskSave() {
-        await setItem('tasks', JSON.stringify(tasks));
-}
-
-async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json());
-}
-
-async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json());
-}
-
 
 
