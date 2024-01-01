c5384da8207307fa501fa1e4aef4b1d304213741
Silvan Stuber <silvan.stuber93@gmail.com>
2024-01-01 20:46:24 +0100

    contact included

--- js/board.js
+++ js/board.js
@@ -1,8 +1,11 @@
+async function boardInit() {
+    await includeHTML();
+    render();
+}
 
 function renderBoardHTML() {
     document.getElementById('contentJoin').innerHTML = ``;
     document.getElementById('contentJoin').innerHTML = generateBoardHTML();
-    render();
 }
 
 function render() {
