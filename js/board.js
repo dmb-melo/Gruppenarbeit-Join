d98b3da84926c315caad75faf1298e66351e113d
Bekir Labjani <127668660+BekirLabjani@users.noreply.github.com>
2024-01-02 16:09:10 +0100

    board

--- js/board.js
+++ js/board.js
@@ -2,8 +2,40 @@
 function renderBoardHTML() {
     document.getElementById('contentJoin').innerHTML = ``;
     document.getElementById('contentJoin').innerHTML = generateBoardHTML();
- 
+    boardInit();
+}
+
+
+async function boardInit() {
+    renderBoradTask();
+}
+
+function renderBoradTask() {
+    let todo = document.getElementById('todo');
+    todo.innerHTML = '';
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
 }
 
 
 
+//     todo.innerHTML = /*html*/`
+//   <div class="theTask cardA">
+//     <div class="category">
+//         <p>User Story</p>
+//     </div>
+//     <div class="title">
+//         <p>Die Kochwelt</p>
+//     </div>
+//     <div class="descraption">
+//         <p>der Test der Aufgabe</p>
+//     </div>
+//     <div class="progress"></div>
+//     <div class="contactsprio">
+//         <div class="contactsCont"></div>
+//         <div class="priorityCont"></div>
+//     </div>
+//   </div>  
+// `;
+}
